8ed5e27520c688817b272651e8f1af6edf918160
Samuel Rojas <samuel.rojasp@gmail.com>
2020-05-23 16:30:35 -0400

    login con celular o email

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -25,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/myaccount';
+    protected $redirectTo = '/admin2/dashboard';
 
     /**
      * Create a new controller instance.
@@ -35,5 +36,45 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+    * Get the login username to be used by the controller.
+    *
+    * @return string
+    */
+    public function username()
+    {
+        $login = request()->input('identity');
+
+        $field = filter_var($login, FILTER_VALIDATE_EMAIL) ? 'email' : 'telephone';
+        request()->merge([$field => $login]);
+
+        return $field;
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function validateLogin(Request $request)
+    {
+        $messages = [
+            'identity.required' => 'Email o teléfono no puede venir vacio',
+            'email.exists' => 'Email no existe',
+            'telephone.exists' => 'El teléfono no existe',
+            'password.required' => 'Password no puede venir vacio',
+        ];
+
+        $request->validate([
+            'identity' => 'required|string',
+            'password' => 'required|string',
+            'email' => 'string|exists:users',
+            'telephone' => 'string|exists:users',
+        ], $messages);
     }
 }
